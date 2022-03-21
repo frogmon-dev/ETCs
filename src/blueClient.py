34274f43c6b6069316ee0b37be9c6ea044a755a9
frogmon <frogmon@frogmon.com>
2022-03-21 16:04:52 +0900

    input change

--- src/blueClient.py
+++ src/blueClient.py
@@ -3,15 +3,20 @@ A simple Python script to send messages to a sever over Bluetooth
 using PyBluez (with Python 2).
 """
 
+import string
 import bluetooth
 
 serverMACAddress = 'E4:5F:01:65:6C:05'
 port = 3
 s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 s.connect((serverMACAddress, port))
+cnt = 0
 while 1:
-    text = raw_input() # Note change to the old (Python 2) raw_input
+    cnt = cnt + 1
+    text = '%d' % cnt
+    sleep(1) 
+    #raw_input() # Note change to the old (Python 2) raw_input
     if text == "quit":
         break
     s.send(text)
-sock.close()
+s.close()
