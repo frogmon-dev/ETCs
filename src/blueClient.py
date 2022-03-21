5642d2061ec2a70892a16780086745b7a6347abd
frogmon <frogmon@frogmon.com>
2022-03-21 15:41:28 +0900

    save server mac addr

--- src/blueClient.py
+++ src/blueClient.py
@@ -5,13 +5,13 @@ using PyBluez (with Python 2).
 
 import bluetooth
 
-serverMACAddress = '00:1f:e1:dd:08:3d'
+serverMACAddress = 'E4:5F:01:65:6C:05'
 port = 3
 s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 s.connect((serverMACAddress, port))
 while 1:
     text = raw_input() # Note change to the old (Python 2) raw_input
     if text == "quit":
-    break
+        break
     s.send(text)
 sock.close()
