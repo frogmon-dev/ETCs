b925168bdb6e3631a38d1b36bfceca2a49d82ad4
frogmon <frogmon@frogmon.com>
2022-03-21 16:08:46 +0900

    수정

--- src/blueClient.py
+++ src/blueClient.py
@@ -13,11 +13,7 @@ s.connect((serverMACAddress, port))
 cnt = 0
 while 1:
     cnt = cnt + 1
-<<<<<<< HEAD
     text = '%d' % cnt
-=======
-    text = string(cnt)
->>>>>>> 1201f22f7688a846648e31d60464d43be2fcfb0c
     sleep(1) 
     #raw_input() # Note change to the old (Python 2) raw_input
     if text == "quit":
