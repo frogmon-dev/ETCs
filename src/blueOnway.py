fbfaec4947c97ffcae5a9268cb52674b363b20dc
frogmon <frogmon@frogmon.com>
2022-03-21 20:03:21 +0900

    retry

--- src/blueOnway.py
+++ src/blueOnway.py
@@ -1,67 +1,101 @@
-# -*- coding: utf-8 -*- 
+import dbus
+import dbus.service
+import dbus.mainloop.glib
+from gi.repository import GLib
 
-#프로그램 시작
-	
-print('')
-print('--------------------------------------------------')
-print('**  Welcome to FROGMON corp.')
-print("**  Let's make it together")
-print("**  ")
-print('--------------------------------------------------')
-print('')
+BUS_NAME = 'org.bluez'
+ADAPTER_IFACE = 'org.bluez.Adapter1'
+ADAPTER_ROOT = '/org/bluez/hci'
+AGENT_IFACE = 'org.bluez.Agent1'
+AGNT_MNGR_IFACE = 'org.bluez.AgentManager1'
+AGENT_PATH = '/my/app/agent'
+AGNT_MNGR_PATH = '/org/bluez'
+CAPABILITY = 'KeyboardDisplay'
+DEVICE_IFACE = 'org.bluez.Device1'
+dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+bus = dbus.SystemBus()
+
+def set_trusted(path):
+    props = dbus.Interface(bus.get_object(BUS_NAME, path), dbus.PROPERTIES_IFACE)
+    props.Set(DEVICE_IFACE, "Trusted", True)
+
+class Agent(dbus.service.Object):
+
+    @dbus.service.method(AGENT_IFACE,
+                         in_signature="", out_signature="")
+    def Release(self):
+        print("Release")
+
+    @dbus.service.method(AGENT_IFACE,
+                         in_signature='o', out_signature='s')
+    def RequestPinCode(self, device):
+        print(f'RequestPinCode {device}')
+        return '0000'
+
+    @dbus.service.method(AGENT_IFACE,
+                         in_signature="ou", out_signature="")
+    def RequestConfirmation(self, device, passkey):
+        print("RequestConfirmation (%s, %06d)" % (device, passkey))
+        set_trusted(device)
+        return
+
+    @dbus.service.method(AGENT_IFACE,
+                         in_signature="o", out_signature="")
+    def RequestAuthorization(self, device):
+        print("RequestAuthorization (%s)" % (device))
+        auth = input("Authorize? (yes/no): ")
+        if (auth == "yes"):
+            return
+        raise Rejected("Pairing rejected")
+
+    @dbus.service.method(AGENT_IFACE,
+                         in_signature="o", out_signature="u")
+    def RequestPasskey(self, device):
+        print("RequestPasskey (%s)" % (device))
+        set_trusted(device)
+        passkey = input("Enter passkey: ")
+        return dbus.UInt32(passkey)
+
+    @dbus.service.method(AGENT_IFACE,
+                         in_signature="ouq", out_signature="")
+    def DisplayPasskey(self, device, passkey, entered):
+        print("DisplayPasskey (%s, %06u entered %u)" %
+              (device, passkey, entered))
+
+    @dbus.service.method(AGENT_IFACE,
+                         in_signature="os", out_signature="")
+    def DisplayPinCode(self, device, pincode):
+        print("DisplayPinCode (%s, %s)" % (device, pincode))
 
 
-import struct
-from bluepy.btle import Scanner, DefaultDelegate, UUID, Peripheral
-
-TARGET_UUID = "0000fe95-0000-1000-8000-00805f9b34fb"
-target_dev = None    
-
-#############################################
-# Define scan callback
-#############################################
-class ScanDelegate(DefaultDelegate):
-    def __init__(self):
-        DefaultDelegate.__init__(self)
-
-    def handleDiscovery(self, dev, isNewDev, isNewData):
-        if isNewDev:
-            print("Discovered device %s" % dev.addr)
-        elif isNewData:
-            print("Received new data from %s" % dev.addr)
-
-#############################################
-# Define notification callback
-#############################################
-class NotifyDelegate(DefaultDelegate):
-    #Constructor (run once on startup)  
-    def __init__(self, params):
-        DefaultDelegate.__init__(self)
-      
-    #func is caled on notifications
-    def handleNotification(self, cHandle, data):
-         print("Notification : " + data.decode("utf-8"))
-
-#############################################
-# Main starts here
-#############################################
-scanner = Scanner().withDelegate(ScanDelegate())
-devices = scanner.scan(10.0)
-cnt = 0
-dev_str=''
+class Adapter:
+    def __init__(self, idx=0):
+        bus = dbus.SystemBus()
+        self.path = f'{ADAPTER_ROOT}{idx}'
+        self.adapter_object = bus.get_object(BUS_NAME, self.path)
+        self.adapter_props = dbus.Interface(self.adapter_object,
+                                            dbus.PROPERTIES_IFACE)
+        self.adapter_props.Set(ADAPTER_IFACE,
+                               'DiscoverableTimeout', dbus.UInt32(0))
+        self.adapter_props.Set(ADAPTER_IFACE,
+                               'Discoverable', True)
+        self.adapter_props.Set(ADAPTER_IFACE,
+                               'PairableTimeout', dbus.UInt32(0))
+        self.adapter_props.Set(ADAPTER_IFACE,
+                               'Pairable', True)
 
 
-for dev in devices:
-    print("Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi))
-    for (adtype, desc, value) in dev.getScanData():
-        # Check iBeacon UUID
-        # 255 is manufacturer data (1  is Flags, 9 is Name)
-        print("  (AD Type=%d) %s = %s" % (adtype, desc, value))
-        if adtype is 2 and TARGET_UUID in value:
-            cnt = cnt+1
-            target_dev = dev
-            print("%d) Device %s (%s), RSSI=%d dB" % (cnt, dev.addr, dev.addrType, dev.rssi))
-            dev_str = "sensor%02d" % (cnt)
+if __name__ == '__main__':
+    agent = Agent(bus, AGENT_PATH)
+    agnt_mngr = dbus.Interface(bus.get_object(BUS_NAME, AGNT_MNGR_PATH),
+                               AGNT_MNGR_IFACE)
+    agnt_mngr.RegisterAgent(AGENT_PATH, CAPABILITY)
+    agnt_mngr.RequestDefaultAgent(AGENT_PATH)
 
-print("Close app")
-
+    adapter = Adapter()
+    mainloop = GLib.MainLoop()
+    try:
+        mainloop.run()
+    except KeyboardInterrupt:
+        agnt_mngr.UnregisterAgent(AGENT_PATH)
+        mainloop.quit()
