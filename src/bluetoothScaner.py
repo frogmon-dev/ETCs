9ec67ef3bf0fccf264cf1759503a52eb9c047e55
frogmon <frogmon@frogmon.com>
2022-03-21 14:53:43 +0900

    new test

---
+++ src/bluetoothScaner.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*- 
+
+print('')
+print('--------------------------------------------------')
+print('**  Program Start **')
+print('--------------------------------------------------')
+print('')
+
+from bluepy.btle import Scanner, DefaultDelegate, UUID, Peripheral
+
+TARGET_UUID = "0000fe95-0000-1000-8000-00805f9b34fb"
+
+
